626f42db61a04b3ed139675289d357a64ea028dd
Tolo789 <cmutti@student.42.fr>
2017-12-07 19:17:15 +0100

    buttons change in stones

---
+++ gomoku/Assets/PutStone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutStone : MonoBehaviour {
+	[HideInInspector]
+	public GameManager gameManager;
+
+	public void TryToPutStone() {
+		if (gameManager == null)
+			return ;
+
+		gameManager.TryPutStone(gameObject);
+	}
+}
--- gomoku/Assets/Scripts/GameManager.cs
+++ gomoku/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public GameObject[] stones;
+	public Sprite[] stoneSprites;
+
 	public GameObject emptyButton;
 
 	public GameObject startBoard;
@@ -18,25 +21,25 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float width = startBoard.GetComponent<RectTransform>().rect.width * canvas.transform.localScale.x;
-		float height = startBoard.GetComponent<RectTransform>().rect.height * canvas.transform.localScale.x;
+		// init board with hidden buttons
+		float width = startBoard.GetComponent<RectTransform>().rect.width ;
+		float height = startBoard.GetComponent<RectTransform>().rect.height;
 		Vector3 startPos = startBoard.transform.position;
-		startPos.x -= width / 2;
-		startPos.y += height / 2;
-		// Vector3 pos = startBoard.transform.position;
-		Debug.Log(canvas.transform.localScale.x);
-		// Debug.Log(width);
-		float step = width / (size - 1);
-		Debug.Log("step: " + step);
-		// pos.x = pos.x + step;
+		startPos.x -= width * canvas.transform.localScale.x / 2;
+		startPos.y += height * canvas.transform.localScale.x / 2;
+		float step = width * canvas.transform.localScale.x / (size - 1);
+		float buttonSize = width / (size - 1);
 		int x = 0;
 		int y = 0;
 		Vector3 tmpPos = startPos;
 		while (y < size) {
 			while (x < size) {
 				GameObject newButton = Instantiate(emptyButton, tmpPos, Quaternion.identity);
+				newButton.name = y + "-" + x;
 				newButton.transform.SetParent(startBoard.transform);
 				newButton.transform.localScale = emptyButton.transform.localScale;
+				newButton.GetComponent<RectTransform>().sizeDelta = new Vector2(buttonSize, buttonSize);
+				newButton.GetComponent<PutStone>().gameManager = this;
 				x++;
 				tmpPos.x += step;
 			}
@@ -49,13 +52,20 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			Debug.Log("Mouse click");
-			Vector3 pos = Input.mousePosition;
-			pos = Camera.main.ScreenToWorldPoint(pos);
-			pos.z = 0;
-			// Instantiate(stones[0], pos, Quaternion.identity);
+		// if (Input.GetMouseButtonDown(0)) {
+		// 	Debug.Log("Mouse click");
+		// 	Vector3 pos = Input.mousePosition;
+		// 	pos = Camera.main.ScreenToWorldPoint(pos);
+		// 	pos.z = 0;
+		// 	// Instantiate(stones[0], pos, Quaternion.identity);
 
-		}
+		// }
+	}
+
+	public void TryPutStone(GameObject button) {
+		Debug.Log(button.name);
+		button.GetComponent<Image>().sprite = stoneSprites[playerPlaying];
+		playerPlaying = (playerPlaying + 1) % stoneSprites.Length;
+		button.GetComponent<PutStone>().enabled = false;
 	}
 }
