5bf30bcdb542d80e63d1ac9df987eb348c9c916f
Tolo789 <cmutti@student.42.fr>
2017-12-07 13:07:07 +0100

    first push

---
+++ gomoku/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject stones;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Mouse click");
+			Vector3 pos = Input.mousePosition;
+			pos = Camera.main.ScreenToWorldPoint(pos);
+			pos.z = 0;
+			Instantiate(stones, pos, Quaternion.identity);
+		}
+	}
+}
