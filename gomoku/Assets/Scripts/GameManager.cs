17b35e511f7ec9784e59f1b7bbd1c18ce9e753b7
Tolo789 <cmutti@student.42.fr>
2017-12-07 17:26:10 +0100

    buttons overlay

--- gomoku/Assets/Scripts/GameManager.cs
+++ gomoku/Assets/Scripts/GameManager.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject stones;
+	public GameObject[] stones;
+	public GameObject emptyButton;
+
+	public GameObject startBoard;
+
+	public Canvas canvas;
+
+	public int size = 19;
+
+	private int playerPlaying = 0;
+	private int[][] map;
 
 	// Use this for initialization
 	void Start () {
+		float width = startBoard.GetComponent<RectTransform>().rect.width * canvas.transform.localScale.x;
+		float height = startBoard.GetComponent<RectTransform>().rect.height * canvas.transform.localScale.x;
+		Vector3 startPos = startBoard.transform.position;
+		startPos.x -= width / 2;
+		startPos.y += height / 2;
+		// Vector3 pos = startBoard.transform.position;
+		Debug.Log(canvas.transform.localScale.x);
+		// Debug.Log(width);
+		float step = width / (size - 1);
+		Debug.Log("step: " + step);
+		// pos.x = pos.x + step;
+		int x = 0;
+		int y = 0;
+		Vector3 tmpPos = startPos;
+		while (y < size) {
+			while (x < size) {
+				GameObject newButton = Instantiate(emptyButton, tmpPos, Quaternion.identity);
+				newButton.transform.SetParent(startBoard.transform);
+				newButton.transform.localScale = emptyButton.transform.localScale;
+				x++;
+				tmpPos.x += step;
+			}
+			y++;
+			tmpPos.y -= step;
+			tmpPos.x = startPos.x;
+			x = 0;
+		}
 	}
 	
 	// Update is called once per frame
@@ -17,7 +54,8 @@ public class GameManager : MonoBehaviour {
 			Vector3 pos = Input.mousePosition;
 			pos = Camera.main.ScreenToWorldPoint(pos);
 			pos.z = 0;
-			Instantiate(stones, pos, Quaternion.identity);
+			// Instantiate(stones[0], pos, Quaternion.identity);
+
 		}
 	}
 }
