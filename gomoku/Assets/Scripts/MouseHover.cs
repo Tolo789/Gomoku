d235fcf66224d9c340400fb0c6b36636c653a912
Hamza LOUAR <hlouar@e1r4p4.42.fr>
2017-12-21 18:39:44 +0100

    added assets for buttons

---
+++ gomoku/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour {
+    public Color StartColor;
+    public Color MouseOverColor;
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    public void OnMouseEnter(){
+        GetComponent<Button>().GetComponentInChildren<Text>().color= MouseOverColor;
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    public void OnMouseExit(){
+        GetComponent<Button>().GetComponentInChildren<Text>().color= StartColor;
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+    
+}
