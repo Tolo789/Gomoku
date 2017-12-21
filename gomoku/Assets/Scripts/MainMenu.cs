bf95c469fa42c88f9136d3e21d0ee120d461637c
Hamza LOUAR <hlouar@e1r4p4.42.fr>
2017-12-21 18:39:18 +0100

    UI hover ok

--- gomoku/Assets/Scripts/MainMenu.cs
+++ gomoku/Assets/Scripts/MainMenu.cs
@@ -5,19 +5,8 @@ using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour {
-	[HideInInspector]
-	public GameManager gameManager;
-
-    public Button playButton;
-    public GameObject settingsButton;
-    public GameObject quitButton;
-
-    void Start()
-    {
-        Button btn = playButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
-    }
-    void TaskOnClick() {
+    public void TaskOnClick() {
+        Debug.Log("titi");
         Application.LoadLevel("Main");
     }
 }
