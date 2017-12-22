3c7c55eda605bd138a2c126d01765a9c0af27227
Hamza LOUAR <hlouar@e1r4p1.42.fr>
2017-12-22 12:21:17 +0100

    MainMenu btn play and exit working

--- gomoku/Assets/Scripts/MainMenu.cs
+++ gomoku/Assets/Scripts/MainMenu.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour {
-    public void TaskOnClick() {
-        Debug.Log("titi");
-        Application.LoadLevel("Main");
+    public void PlayClick() {
+        SceneManager.LoadScene("Main");
+    }
+    public void ExitClick() {
+        Application.Quit();
     }
 }
