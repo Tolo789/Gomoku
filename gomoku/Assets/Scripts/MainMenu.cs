0c90a29a0dd5a4eaf0b96d8ff831e18bd6ea49cb
Hamza LOUAR <hlouar@e1r4p1.42.fr>
2017-12-22 16:52:36 +0100

    MainMenu + playmenu ok

--- gomoku/Assets/Scripts/MainMenu.cs
+++ gomoku/Assets/Scripts/MainMenu.cs
@@ -6,10 +6,18 @@ using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour {
-    public void PlayClick() {
-        SceneManager.LoadScene("Main");
+
+    public void PlayClick(GameObject playSettings) {
+        playSettings.SetActive(true);
     }
     public void ExitClick() {
         Application.Quit();
+    }
+    public void GoClick(GameObject playSettings) {
+        playSettings.SetActive(true);
+        SceneManager.LoadScene("Main");
+    }
+     public void GoBack(GameObject playSettings) {
+       playSettings.SetActive(false);
     }
 }
