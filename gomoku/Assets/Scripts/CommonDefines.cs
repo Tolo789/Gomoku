aedfa79aa04ae7cdd4e8172014e08227728d9048
Hamza LOUAR <hlouar@e1r4p1.42.fr>
2017-12-22 18:31:54 +0100

    added CommonDefines

---
+++ gomoku/Assets/Scripts/CommonDefines.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonDefines {
+	public const string VERSUS_IA = "VersusIA";
+	public const string FIRST_PLAYER_PLAYING = "FirstPlayer";
+}
